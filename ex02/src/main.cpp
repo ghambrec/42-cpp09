174fd129f197c66c94ef284c76a2a18905bdcaf7
Gabriel Hambrecht <ghambrec@2-C-9.42heilbronn.de>
2026-03-03 15:02:31 +0100

    started ex02 with parse_argument

---
+++ ex02/src/main.cpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghambrec <ghambrec@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/03 14:12:11 by ghambrec          #+#    #+#             */
+/*   Updated: 2026/03/03 15:02:08 by ghambrec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+static int parse_argument(const std::string& str)
+{
+	static std::regex REGEX{R"(^[+-]?\d+$)"};
+	std::smatch match;
+	if (!std::regex_match(str, match, REGEX))
+		throw std::runtime_error("Error: Detected a non number value!");
+	
+	long long ln = std::stoll(str);
+	if (ln <= 0)
+		throw std::runtime_error("Error: Detected number smaller equals 0");
+	if (ln > std::numeric_limits<int>::max())
+		throw std::runtime_error("Error: Detected number bigger than INT_MAX");
+	return (static_cast<int>(ln));
+}
+
+int main(int argc, char** argv)
+{
+	if (argc < 3)
+	{
+		std::cerr << "Error: Minimum 2 arguments needed!\n";
+		return (1);
+	}
+
+	// create vector with int numbers
+	std::vector<int> v;
+	int i = 1;
+	try
+	{
+		while (argv[i])
+		{
+			v.push_back(parse_argument(argv[i]));
+			i++;
+		}
+
+		// print given numbers
+		std::cout << "Numbers: ";
+		for (int n : v)
+		{
+			std::cout << n << " ";	
+		}
+		std::cout << "\n";
+	
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+
+}
