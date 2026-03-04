32356e2bf451d00591c22e6f3d6fc9ba6cc08e57
Gabriel Hambrecht <ghambrec@2-C-9.42heilbronn.de>
2026-03-04 13:06:44 +0100

    renamed pair_size to element_size

--- ex02/inc/PmergeMe.hpp
+++ ex02/inc/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: ghambrec <ghambrec@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 14:12:38 by ghambrec          #+#    #+#             */
-/*   Updated: 2026/03/04 12:51:13 by ghambrec         ###   ########.fr       */
+/*   Updated: 2026/03/04 13:06:41 by ghambrec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 #include <limits>
 
 void print_vector(const std::vector<int>& v);
-void fjalgo(std::vector<int>& v, size_t pair_size);
+void fjalgo(std::vector<int>& v, size_t element_size);
 
 
 // [6 15 8 16 2 11 0 17 , 9 18 14 19 3 10 1 21] 5 12 4 20 7 13
--- ex02/src/PmergeMe.cpp
+++ ex02/src/PmergeMe.cpp
@@ -6,30 +6,30 @@
 /*   By: ghambrec <ghambrec@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 14:12:36 by ghambrec          #+#    #+#             */
-/*   Updated: 2026/03/04 12:52:30 by ghambrec         ###   ########.fr       */
+/*   Updated: 2026/03/04 13:06:18 by ghambrec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-void fjalgo(std::vector<int>& v, size_t pair_size)
+void fjalgo(std::vector<int>& v, size_t element_size)
 {
-	if ((v.size() / pair_size) < 2)
+	if ((v.size() / element_size) < 2)
 		return ;
 	
 	// step 1 - build and sort pairs
-	size_t count_elements = v.size() / pair_size;
+	size_t count_elements = v.size() / element_size;
 	size_t count_pairs = count_elements / 2;	
 	for (size_t pair = 0; pair < count_pairs; pair++)
 	{
-		size_t start_left = (2 * pair) * pair_size;
-		size_t start_right = (2 * pair + 1) * pair_size;
+		size_t start_left = (2 * pair) * element_size;
+		size_t start_right = (2 * pair + 1) * element_size;
 		size_t key_left = start_right - 1;
-		size_t key_right = start_right + pair_size - 1;
+		size_t key_right = start_right + element_size - 1;
 
 		if (v[key_left] > v[key_right]) // vergleich
 		{
-			for (size_t i = 0; i < pair_size; i++)
+			for (size_t i = 0; i < element_size; i++)
 			{
 				std::swap(v[start_left + i], v[start_right + i]);
 			}
@@ -38,18 +38,18 @@ void fjalgo(std::vector<int>& v, size_t pair_size)
 
 	// // printing for debug
 	// size_t pair = 0;
-	// size_t start_left = (2 * pair) * pair_size;
-	// size_t start_right = (2 * pair + 1) * pair_size;
+	// size_t start_left = (2 * pair) * element_size;
+	// size_t start_right = (2 * pair + 1) * element_size;
 	// size_t key_left = start_right - 1;
-	// size_t key_right = start_right + pair_size - 1;
-	// std::cout << "pairsize: " << pair_size << std::endl;
+	// size_t key_right = start_right + element_size - 1;
+	// std::cout << "pairsize: " << element_size << std::endl;
 	// std::cout << "- start l/f: " << start_left << "/" << start_right << std::endl;
 	// std::cout << "- key l/f:   " << key_left << "/" << key_right << std::endl;
 	// std::cout << "- elements: " << count_elements << "; pairs: " << count_pairs << std::endl;
 	std::cout << "- after sorting pairs ";
 	print_vector(v);
 	
-	fjalgo(v, pair_size * 2);
+	fjalgo(v, element_size * 2);
 
 	// step 2 - build main and pend
 	std::vector<int> main;
@@ -57,31 +57,40 @@ void fjalgo(std::vector<int>& v, size_t pair_size)
 
 	for (size_t pair = 0; pair < count_pairs; pair++)
 	{
-		size_t start_left = (2 * pair) * pair_size;
-		size_t start_right = (2 * pair + 1) * pair_size;
+		size_t start_left = (2 * pair) * element_size;
+		size_t start_right = (2 * pair + 1) * element_size;
 
 		if (pair == 0) // b1 kopieren
-			main.insert(main.end(), v.begin() + start_left, v.begin() + start_left + pair_size);
+			main.insert(main.end(), v.begin() + start_left, v.begin() + start_left + element_size);
 		else
-			pend.insert(pend.end(), v.begin() + start_left, v.begin() + start_left + pair_size);
+			pend.insert(pend.end(), v.begin() + start_left, v.begin() + start_left + element_size);
 
 		// restlichen a's kopieren
-		main.insert(main.end(), v.begin() + start_right, v.begin() + start_right + pair_size);
+		main.insert(main.end(), v.begin() + start_right, v.begin() + start_right + element_size);
 	}
 	// ggf. unpaired element mit in pend aufnehmen
 	if (count_elements % 2 == 1)
 	{
-		size_t start_missing = (count_elements - 1) * pair_size;
-		pend.insert(pend.end(), v.begin() + start_missing, v.begin() + start_missing + pair_size);
+		size_t start_missing = (count_elements - 1) * element_size;
+		pend.insert(pend.end(), v.begin() + start_missing, v.begin() + start_missing + element_size);
 	}
+
+	std::vector<int> rest;
+	size_t anzahl_rest = v.size() - ((element_size * 2) * count_pairs);
+	if (anzahl_rest > 0)
+		rest.insert(rest.begin(), v.end() - anzahl_rest, v.end());
 	
 
-	std::cout << "pairsize: " << pair_size << std::endl;
+	std::cout << "pairsize: " << element_size << std::endl;
 	std::cout << "- elements: " << count_elements << "; pairs: " << count_pairs << std::endl;
 	std::cout << "- mainchain ";
 	print_vector(main);
 	std::cout << "- pendchain ";
 	print_vector(pend);
+	std::cout << "- restchain ";
+	print_vector(rest);
+
+	// step 3 - insert pend into main
 	
 		
 }
